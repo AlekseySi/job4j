6c972b617bafc205667c682bae933e65b089e9ef
Aleksey Siminko <alekseysimink@gmail.com>
2018-07-19 13:23:49 +0300

    1.3.1 Maven Plugins [#49546]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -2,7 +2,9 @@ package ru.job4j.calculate;
 
 /**
   *Class Calculate решение задачи части 001 урок1.
-  *@author Siminko
+  *
+  *@author Siminko (mailto:alekseysimink@gmail.com)
+  *version $Id$
   *@since 14.07.2018
 */
 public class Calculate {
@@ -13,10 +15,13 @@ public class Calculate {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
-}
 
     /**
 	*Metod echo.
 	*@param name Aleksey 
 	*@return Echo plus Aleksey
 	*/
+	public String echo(Spring name){
+		return "Echo, echo, echo : " + name;
+	}
+}
