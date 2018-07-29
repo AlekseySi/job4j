57d8364df897142d4bc71b7e43c9852f566cbaa1
Aleksey Siminko <alekseysimink@gmail.com>
2018-07-29 23:37:15 +0300

    3.1. Элементарный калькулятор. [#185]

---
+++ chapter_001/src/main/java/ru/job4j/calculator/calculator.java
@@ -0,0 +1,33 @@
+package ru.job4j.calculator;
+
+public class Calculator {
+    private double result;
+
+    public double getResult() {
+        return this.result;
+    }
+public void add(double first, double second) {
+        this.result = first + second;
+    }
+	public void subtract(double first, double second) {
+        this.result = first - second;
+    }
+
+    public void multiple(double first, double second) {
+        this.result = first * second;
+    }
+
+    public void div(double first, double second) {
+        if (second == 0d) {
+            throw new IllegalStateException("Div by Zero");
+        }
+        this.result = first / second;
+    }
+
+    public void expand(double first, double second) {
+        this.result = first;
+        for (int index=1;index!=second;index++) {
+            this.result *= first;
+        }
+    }
+}
