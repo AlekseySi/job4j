b944eb65653b774cd77030443c096fe34a63119f
Aleksey Siminko <alekseysimink@gmail.com>
2018-07-30 21:49:03 +0300

    3.1. Элементарный калькулятор.2

--- chapter_001/src/main/java/ru/job4j/calculator/calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/calculator.java
@@ -3,19 +3,14 @@ package ru.job4j.calculator;
 public class Calculator {
     private double result;
 
-    public double getResult() {
-        return this.result;
-    }
-public void add(double first, double second) {
-        this.result = first + second;
-    }
-	public void subtract(double first, double second) {
-        this.result = first - second;
+    public double getResult() { return this.result;
     }
 
-    public void multiple(double first, double second) {
-        this.result = first * second;
-    }
+    public void add(double first, double second) {this.result = first + second; }
+
+    public void subtract(double first, double second) {this.result = first - second; }
+
+    public void multiple(double first, double second) {this.result = first * second; }
 
     public void div(double first, double second) {
         if (second == 0d) {
--- chapter_001/src/main/java/ru/job4j/calculator/calculatorTest.java
+++ chapter_001/src/main/java/ru/job4j/calculator/calculatorTest.java
@@ -14,4 +14,45 @@ public class CalculatorTest {
         double expected = 2D;
         assertThat(result, is(expected));
     }
+	@Test
+    public void whenSubtractOneOnOneThenResultZero() throws Exception {
+        Calculator calc = new Calculator();
+        calc.subtract(1d, 1d);
+        assertThat(calc.getResult(), is(0d));
+    }
+
+    @Test
+    public void whenMultipleOneOnOneThenResultOne() throws Exception {
+        Calculator calc = new Calculator();
+        calc.multiple(1d, 1d);
+        assertThat(calc.getResult(), is(1d));
+    }
+
+    @Test
+    public void whenDivOneOnOneThenResultOne() throws Exception {
+        Calculator calc = new Calculator();
+        calc.div(1d, 1d);
+        assertThat(calc.getResult(), is(1d));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void whenDivByZeroThenException() throws Exception {
+        Calculator calc = new Calculator();
+        calc.div(1d, 0d);
+    }
+
+    @Test
+    public void whenExpandOneOnOneThenResultOne() throws Exception {
+        Calculator calc = new Calculator();
+        calc.expand(1d, 1d);
+        assertThat(calc.getResult(), is(1d));
+    }
+
+    @Test
+    public void whenExpandTwoOnTwoThenResultFour() throws Exception {
+        Calculator calc = new Calculator();
+        calc.expand(2d, 2d);
+        assertThat(calc.getResult(), is(4d));
+    }
+
 }
